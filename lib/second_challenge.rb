d23f3361e3cbaae074645dd3ba550c72755fc8a4
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-04 23:23:44 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      :pstriarch =>
-      :matriarch
-      :hero 
-      :hero_friends
+      
+      :pstriarch => {},
+      :matriarch =>{},
+      :hero => {},
+      :hero_friends =>[],
       }, 
     :capulet => {
-      #your key/value pairs here!
-      :patriarch =>
-      :matriarch
-      :heroine
-      :heroine_friends
+      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine =>{},
+      :heroine_friends =>[]
       }
 
   
