c64fb5fb48c23bcf245167d02e1ff9129517fdb3
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-04 23:49:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,9 +24,14 @@ def third_challenge
         :age => "50"
       },
       :matriarch => {
-        :name => ""
+        :name => "Lady Capulet",
+        :age => "51"
       },
-      :heroine =>{},
+      :heroine =>{
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
       :heroine_friends =>[]
       }
 
